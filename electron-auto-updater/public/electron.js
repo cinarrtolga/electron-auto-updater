a4c5f27bce1d66dbce1e8cfc3bcb7448da469238
= <cinarrtolga@gmail.com>
2022-07-06 14:51:01 +0100

    auto updater integration

--- electron-auto-updater/public/electron.js
+++ electron-auto-updater/public/electron.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
+const { autoUpdater } = require("electron-updater");
+
+let updateInterval = null;
 
 function createWindow() {
     const startUrl = process.env.ELECTRON_START_URL || url.format({
@@ -15,7 +18,7 @@ function createWindow() {
             nodeIntegration: true
         }
     });
-    win.loadURL(startUrl);
+    win.loadURL('file:///' + __dirname + "/index.html");
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') {
             app.quit()
@@ -23,7 +26,10 @@ function createWindow() {
     });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+    updateInterval = setInterval(() => autoUpdater.checkForUpdates(), 5000);
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -35,4 +41,30 @@ app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
     }
+});
+
+autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Ok'],
+        title: 'Update Available',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version download started. The app will be restarted to install the update.'
+    };
+    dialog.showMessageBox(dialogOpts);
+
+    updateInterval = null;
+});
+
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Restart', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    };
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    });
 });

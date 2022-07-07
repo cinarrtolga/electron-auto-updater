e0d1df305a9e4fc67f89e7297475532df604db7c
= <cinarrtolga@gmail.com>
2022-07-07 09:33:04 +0100

    release/pre-release updates

--- electron-auto-updater/public/electron.js
+++ electron-auto-updater/public/electron.js
@@ -2,6 +2,9 @@ const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
 const { autoUpdater } = require("electron-updater");
+const fs = require('fs');
+const data = fs.readFileSync(__dirname + '/../package.json', 'utf8');
+const dataObj = JSON.parse(data);
 
 let updateInterval = null;
 
@@ -28,7 +31,47 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow();
-    updateInterval = setInterval(() => autoUpdater.checkForUpdates(), 5000);
+
+    if (dataObj.version.includes("-alpha")) {
+        autoUpdater.channel = "alpha";
+        autoUpdater.allowPrerelease = true;
+    } else if (dataObj.version.includes("-beta")) {
+        autoUpdater.channel = "beta";
+        autoUpdater.allowPrerelease = true;
+    } else {
+        autoUpdater.channel = "latest";
+        autoUpdater.allowPrerelease = false;
+    }
+
+    autoUpdater.autoDownload = false;
+
+    updateInterval = setInterval(() => autoUpdater.checkForUpdatesAndNotify().then((updateCheckResult) => {
+        if (updateCheckResult.updateInfo.version !== dataObj.version) {
+            if (updateCheckResult.updateInfo.version.includes("-alpha") && dataObj.version.includes("-alpha")) {
+                dialog.showMessageBox({
+                    type: 'info',
+                    title: 'Update available',
+                    message: 'An update is available. Do you want to update now?',
+                    buttons: ['Yes', 'No']
+                }).then((result) => {
+                    if (result.response === 0) {
+                        autoUpdater.downloadUpdate();
+                    }
+                });
+            } else if (!updateCheckResult.updateInfo.version.includes("-alpha") && !dataObj.version.includes("-alpha")) {
+                dialog.showMessageBox({
+                    type: 'info',
+                    title: 'Update available',
+                    message: 'An update is available. Do you want to update now?',
+                    buttons: ['Yes', 'No']
+                }).then((result) => {
+                    if (result.response === 0) {
+                        autoUpdater.downloadUpdate();
+                    }
+                });
+            }
+        }
+    }), 5000);
 });
 
 app.on('window-all-closed', () => {
@@ -49,20 +92,22 @@ autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
         buttons: ['Ok'],
         title: 'Update Available',
         message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail: 'A new version download started. The app will be restarted to install the update.'
+        detail: `A new ${autoUpdater.channel} version download started. The app will be restarted to install the update.`
     };
     dialog.showMessageBox(dialogOpts);
 
-    updateInterval = null;
+    clearInterval(updateInterval);
 });
 
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+    clearInterval(updateInterval);
+
     const dialogOpts = {
         type: 'info',
         buttons: ['Restart', 'Later'],
         title: 'Application Update',
         message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+        detail: `A new ${autoUpdater.channel} version has been downloaded. Restart the application to apply the updates.`
     };
     dialog.showMessageBox(dialogOpts).then((returnValue) => {
         if (returnValue.response === 0) autoUpdater.quitAndInstall()

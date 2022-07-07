7007785ec42b6bb7344f133d53446f63b5cb6edd
= <cinarrtolga@gmail.com>
2022-07-07 12:41:56 +0100

    Code cleaned

--- electron-auto-updater/public/electron.js
+++ electron-auto-updater/public/electron.js
@@ -1,7 +1,28 @@
+/*
+*  Auto updater channel feature is not available for GitHub releases.
+*  Because of that, for the second environment, we are going to use the allowPrerelease field.
+*  After doing any development, the app should be builded for dev.
+*  The dev build will create a new version number with -beta suffix. 
+*  After releasing the builded package, it should be released with the pre-release checkbox.
+*  Then the update will be available for beta users. 
+*  After deciding to release the package for live users, the package should be builded and released for prod. 
+*  For the prod release, the pre-release checkbox should not be checked.
+*  Test scenario versions 1.1.2-beta / 1.1.2 / 1.1.3-beta / 1.1.3 / 1.1.4-beta
+*  If you download 1.1.2-beta or 1.1.3-beta, you will see the update popup for 1.1.4-beta. After clicking the update button, the app will be updated to 1.1.4-beta.
+*  If you download 1.1.2, you will see the update popup for 1.1.3. After clicking the update button, the app will be updated to 1.1.3. Not 1.1.4-beta.
+*  Note: After releasing the 1.1.4 live version, the old beta version will not be able to download 1.1.4-beta version anymore. 
+*  The beta users will continue to see the update popup after releasing the next beta version. 
+*/
+
 const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
 const { autoUpdater } = require("electron-updater");
+/*
+*  The following 3 lines is required for to read version number.
+*  The app creates different version number for beta versions.
+*  With this specific version name, it decides to allow pre-releases or not.
+*/
 const fs = require('fs');
 const data = fs.readFileSync(__dirname + '/../package.json', 'utf8');
 const dataObj = JSON.parse(data);
@@ -32,33 +53,42 @@ function createWindow() {
 app.whenReady().then(() => {
     createWindow();
 
-    if (dataObj.version.includes("-alpha")) {
-        autoUpdater.channel = "alpha";
-        autoUpdater.allowPrerelease = true;
-    } else if (dataObj.version.includes("-beta")) {
-        autoUpdater.channel = "beta";
+    /*
+    *  The following condition is required to decide if the pre-release tag is available for package or not.
+    *  Pre-released packages should be available just for beta users.
+    */
+    if (dataObj.version.includes("-beta")) {
         autoUpdater.allowPrerelease = true;
     } else {
-        autoUpdater.channel = "latest";
         autoUpdater.allowPrerelease = false;
     }
 
+    /*
+    *  The app should not download the package automatically. We don't want to download live packages for beta users.
+    */
     autoUpdater.autoDownload = false;
 
+    /*
+    *  This block compares the update version with the current version and decides the action.
+    *  With this block, beta users will have beta updates and live users will have live updates.
+    *  If we don't add this block, beta users will have live updates and it will not be possible to download beta packages again for them because of the allowPrerelease field. 
+    */
     updateInterval = setInterval(() => autoUpdater.checkForUpdatesAndNotify().then((updateCheckResult) => {
         if (updateCheckResult.updateInfo.version !== dataObj.version) {
-            if (updateCheckResult.updateInfo.version.includes("-alpha") && dataObj.version.includes("-alpha")) {
+            if (updateCheckResult.updateInfo.version.includes("-beta") && dataObj.version.includes("-beta")) {
+                clearInterval(updateInterval);
                 dialog.showMessageBox({
                     type: 'info',
                     title: 'Update available',
-                    message: 'An update is available. Do you want to update now?',
+                    message: 'A beta update is available. Do you want to update now?',
                     buttons: ['Yes', 'No']
                 }).then((result) => {
                     if (result.response === 0) {
                         autoUpdater.downloadUpdate();
                     }
                 });
-            } else if (!updateCheckResult.updateInfo.version.includes("-alpha") && !dataObj.version.includes("-alpha")) {
+            } else if (!updateCheckResult.updateInfo.version.includes("-beta") && !dataObj.version.includes("-beta")) {
+                clearInterval(updateInterval);
                 dialog.showMessageBox({
                     type: 'info',
                     title: 'Update available',
@@ -86,19 +116,6 @@ app.on('activate', () => {
     }
 });
 
-autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-        type: 'info',
-        buttons: ['Ok'],
-        title: 'Update Available',
-        message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail: `A new ${autoUpdater.channel} version download started. The app will be restarted to install the update.`
-    };
-    dialog.showMessageBox(dialogOpts);
-
-    clearInterval(updateInterval);
-});
-
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
     clearInterval(updateInterval);
 
@@ -109,6 +126,7 @@ autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
         message: process.platform === 'win32' ? releaseNotes : releaseName,
         detail: `A new ${autoUpdater.channel} version has been downloaded. Restart the application to apply the updates.`
     };
+    
     dialog.showMessageBox(dialogOpts).then((returnValue) => {
         if (returnValue.response === 0) autoUpdater.quitAndInstall()
     });
